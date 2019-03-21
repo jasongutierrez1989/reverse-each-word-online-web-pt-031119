5ecdaefd900cd0e8748de6077535bb3a6c863c46
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-03-21 02:17:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(str)
   array = str. split(' ')
-  array.collect {|word| word.reverse}
-  return array.join
+  array = array.collect {|word| word.reverse}
+  return array.join(' ')
 end
